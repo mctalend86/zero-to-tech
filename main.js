519996955c172a7d99b114cc12d7d059a22cc782
mctalend86 <mctalend@gmail.com>
2026-07-28 09:53:43 +0800

    修复了4173样式和动画丢失的问题

---
+++ main.js
@@ -0,0 +1,14 @@
+// 导入所有样式文件
+import './css/reset.css'
+import './css/variables.css'
+import './css/layout.css'
+import './css/hero.css'
+import './css/nav.css'
+import './css/cards.css'
+import './css/lab.css'
+import './css/responsive.css'
+
+// 导入业务JS模块
+import './js/cards.js'
+import './js/score.js'
+import './js/nav.js'
